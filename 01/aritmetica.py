c2c0fac9bd85f088449870855edfc99bcca793db
Angel Beneman <Angelbeneman801@gmail.com>
2024-08-01 19:46:27 -0300

    Update aritmetica.py

--- 01/aritmetica.py
+++ 01/aritmetica.py
@@ -2,9 +2,9 @@ print('1. Calcula el área de un rectángulo con base 15 y altura 8.\n\nPará ca
 
 base = 15
 altura = 8
-print(' Base =', base, '\n Altura =', altura)
+print(f' Base =', {base}, '\n Altura =', altura)
 
 area_rectangulo = base * altura
 print( '\nYá con ello usamos la formula para calcular.\n Base.Altura = Área')
 
-print('\nEl área del rectángulo es: ' + str(area_rectangulo))
+print(f'\nEl área del rectángulo es: {area_rectangulo}')

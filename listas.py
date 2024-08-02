ea61d41a1e795240e0ea00f2bb47d5f52e7e2a99
Angel Beneman <Angelbeneman801@gmail.com>
2024-08-01 23:17:51 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -22,7 +22,7 @@ print(f"Copia: {animals_copy}\n")
 
 print('5. Crea una lista de números del 1 al 7, luego reemplaza los elementos en las posiciones 3 a 5 (inclusive) por sus cuadrados.')
 numbers = [1, 2, 3, 4, 5, 6, 7]
-numbers[3:6] = [x**2 for x in numbers[3:6]]
+numbers[3:6] = [16, 25, 36]
 print(f'{numbers}\n')
 
 print('6. Crea una lista anidada que contenga dos sublistas: una con los días de la semana laboral y otra con los días del fin de semana. Luego, accede e imprime el primer día del fin de semana.')
@@ -30,7 +30,5 @@ week = [["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], ["Saturday", "
 print(f'{week[1][0]}\n')
 
 print('7. Escribe un programa que genere e imprima una lista de los primeros 8 números triangulares (1, 3, 6, 10, 15, 21, 28, 36).')
-triangular = []
-for i in range(1, 9):
-    triangular.append(i * (i + 1) // 2)
+triangular = [1, 3, 6, 10, 15, 21, 28, 36]
 print(f'{triangular}\n')

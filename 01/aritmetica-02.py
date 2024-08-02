be374f8b3b5743048ddc05d3ed6cafad26259923
Angel Beneman <Angelbeneman801@gmail.com>
2024-08-01 21:51:03 -0300

    Delete 01 directory

--- 01/aritmetica-02.py
+++
@@ -1,8 +0,0 @@
-print('2. Divide 100 entre 6 y muestra el resultado como número decimal.\n\nDefinimos los numeros "100" y "6" para dividir y mostrar el resultado float:')
-
-numerador = 100
-denominador = 6
-
-resultado = numerador / denominador
-
-print(f' El resultado de dividir {numerador} entre {denominador} es: {resultado:.4f}')
